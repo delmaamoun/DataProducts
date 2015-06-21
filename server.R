6ec2ab531a0e87ca2c707ed42e52519c8535a16e
Dina ElMaamoun <dina.elmaamoun@aucegypt.edu>
2015-06-21 23:36:04 +0200

    Create server.R

---
+++ server.R
@@ -0,0 +1,29 @@
+library(shiny)
+library(datasets)
+
+# Define server logic required to summarize and view the selected dataset
+shinyServer(function(input, output) {
+  
+  # Return the requested dataset
+  data(mtcars)
+  form <- reactive({
+    formula1 = switch(input$predictor,
+           "All" = mpg ~.,
+           "cyl" = mpg ~ cyl,
+           "hp" = mpg ~ hp,
+           "disp" = mpg ~ disp,
+           "wt" = mpg ~ wt)
+    glm(formula1,data=mtcars)
+  })
+  
+  # Generate a summary of the dataset
+  output$summary <- renderPrint({
+    model <- form()
+    summary(model)
+  })
+  
+#   # Show the first "n" observations
+#   output$view <- renderTable({
+#     head(datasetInput(), n = input$obs)
+#  })
+})
